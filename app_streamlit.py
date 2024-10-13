660a02e6418af84648f05172bb7d5a7c8e789841
kwsrsn <kawisara.sn@gmail.com>
2024-10-13 14:19:28 +0700

    delete streamlit

--- app_streamlit.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import requests
-
-st.title("Streamlit App")
-
-response = requests.get("http://localhost:5000/api/data")  # เรียกใช้ Flask API
-data = response.json()
-
-st.write(data)  # แสดงข้อมูลจาก Flask
